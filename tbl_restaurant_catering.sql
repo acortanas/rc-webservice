78f8323ee59d6b69e0d7857088a83724cf005ada
Ajeng Setoresmi <39654313+acortanas@users.noreply.github.com>
2019-06-14 23:40:48 +0700

    #3 - restful api using yii2 (with QueryParamAuth)
    see auth-restful-api-yii2.txt and restful-api-yii2.txt for the documentation. 
    README.txt and auth-restful-api-yii2.txt contain same info

--- tbl_restaurant_catering.sql
+++ tbl_restaurant_catering.sql
@@ -1,6 +1,46 @@
-INSERT INTO `tbl_restaurant_catering`(`rc_id`, `rc_name`, `rc_cuisine`, `rc_currency`, `rc_average_cost`, `rc_telp`, `rc_address`, `rc_city`) VALUES (1, 'Catering Rakyat', 'Indonesian, Sunda', 'IDR', 30000, '082111888792', 'Jl. Lincar II No. 17, Bogor Utara', 'Bogor');
-INSERT INTO `tbl_restaurant_catering`(`rc_id`, `rc_name`, `rc_cuisine`, `rc_currency`, `rc_average_cost`, `rc_telp`, `rc_address`, `rc_city`) VALUES (2, 'RM Lily Catering', 'Indonesian-Medan', 'IDR', 35000, '(021) 5678752', 'Komplek Taman Duta Mas, Jl. Tubagus Angke, Jelamba', 'Jakarta');
-INSERT INTO `tbl_restaurant_catering`(`rc_id`, `rc_name`, `rc_cuisine`, `rc_currency`, `rc_average_cost`, `rc_telp`, `rc_address`, `rc_city`) VALUES (3, 'Bunga Rampai', 'Indonesian', 'IDR', 350000, '(021) 31926224', 'Jl. Teuku Cik Ditiro No. 35, Menteng', 'Jakarta');
-INSERT INTO `tbl_restaurant_catering`(`rc_id`, `rc_name`, `rc_cuisine`, `rc_currency`, `rc_average_cost`, `rc_telp`, `rc_address`, `rc_city`) VALUES (4, 'WAKI Japanese BBQ Dining', 'Japanese, Grill, BBQ, Korean, ', 'IDR', 150000, '081290109624', 'Lantai 1, Jl. Tanjung Karang No. 5, Thamrin', 'Jakarta');
-INSERT INTO `tbl_restaurant_catering`(`rc_id`, `rc_name`, `rc_cuisine`, `rc_currency`, `rc_average_cost`, `rc_telp`, `rc_address`, `rc_city`) VALUES (5, 'Claypot Popo', 'Chinese, Asian', 'IDR', 50000, '085959939889', 'Jl. H. Agus Salim No. 23A, Menteng', 'Jakarta');
-INSERT INTO `tbl_restaurant_catering`(`rc_id`, `rc_name`, `rc_cuisine`, `rc_currency`, `rc_average_cost`, `rc_telp`, `rc_address`, `rc_city`) VALUES (6, 'HERITAGE by Tan Goei', 'Belanda, Indonesian', 'IDR', 200000, '(021) 3155057', 'Jl. Teuku Cik Ditiro No. 3, Menteng', 'Jakarta');
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 100138
+ Source Host           : localhost:3306
+ Source Schema         : website
+
+ Target Server Type    : MySQL
+ Target Server Version : 100138
+ File Encoding         : 65001
+
+ Date: 14/06/2019 23:31:33
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tbl_restaurant_catering
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_restaurant_catering`;
+CREATE TABLE `tbl_restaurant_catering`  (
+  `rc_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `rc_name` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `rc_cuisine` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `rc_currency` varchar(5) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `rc_average_cost` int(11) NOT NULL,
+  `rc_telp` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `rc_address` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `rc_city` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`rc_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of tbl_restaurant_catering
+-- ----------------------------
+INSERT INTO `tbl_restaurant_catering` VALUES (1, 'Catering Rakyat', 'Indonesian, Sunda', 'IDR', 30000, '082111888792', 'Jl. Lincar II No. 17, Bogor Utara', 'Bogor');
+INSERT INTO `tbl_restaurant_catering` VALUES (2, 'RM Lily Catering', 'Indonesian-Medan', 'IDR', 35000, '(021) 5678752', 'Komplek Taman Duta Mas, Jl. Tubagus Angke, Jelamba', 'Jakarta');
+INSERT INTO `tbl_restaurant_catering` VALUES (3, 'Bunga Rampai', 'Indonesian', 'IDR', 350000, '(021) 31926224', 'Jl. Teuku Cik Ditiro No. 35, Menteng', 'Jakarta');
+INSERT INTO `tbl_restaurant_catering` VALUES (4, 'WAKI Japanese BBQ Dining', 'Japanese, Grill, BBQ, Korean, ', 'IDR', 150000, '081290109624', 'Lantai 1, Jl. Tanjung Karang No. 5, Thamrin', 'Jakarta');
+INSERT INTO `tbl_restaurant_catering` VALUES (5, 'Claypot Popo', 'Chinese, Asian', 'IDR', 50000, '085959939889', 'Jl. H. Agus Salim No. 23A, Menteng', 'Jakarta');
+INSERT INTO `tbl_restaurant_catering` VALUES (6, 'HERITAGE by Tan Goei', 'Belanda, Indonesian', 'IDR', 200000, '(021) 3155057', 'Jl. Teuku Cik Ditiro No. 3, Menteng', 'Jakarta');
+
+SET FOREIGN_KEY_CHECKS = 1;
--- tbl_user.sql
+++ tbl_user.sql
@@ -1,2 +1,38 @@
-INSERT INTO `tbl_user`(`id`, `username`, `auth_key`, `status`) VALUES (1, 'admin', 'ff4ab0d892ebc9657d3a2aec3cf4a61b', NULL);
-INSERT INTO `tbl_user`(`id`, `username`, `auth_key`, `status`) VALUES (2, 'test', 'dweb32rbhb34235kjn6nk2n3j55j6bjb', NULL);
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 100138
+ Source Host           : localhost:3306
+ Source Schema         : website
+
+ Target Server Type    : MySQL
+ Target Server Version : 100138
+ File Encoding         : 65001
+
+ Date: 14/06/2019 23:32:52
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tbl_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_user`;
+CREATE TABLE `tbl_user`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `auth_key` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `status` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of tbl_user
+-- ----------------------------
+INSERT INTO `tbl_user` VALUES (1, 'admin', 'ff4ab0d892ebc9657d3a2aec3cf4a61b', 10);
+INSERT INTO `tbl_user` VALUES (2, 'test', 'dweb32rbhb34235kjn6nk2n3j55j6bjb', 10);
+
+SET FOREIGN_KEY_CHECKS = 1;
